23eeab3ff0cec0db3303ab72789b36e5d7ad7c27
RatiborM <r_morkvin@vk.com>
2024-12-27 19:18:37 +0300

    исправляю ошибки4

---
+++ gendiff/formatters/__init__.py
@@ -0,0 +1,12 @@
+def get_formatter(format_type):
+    if format_type == 'json':
+        from gendiff.formatters.json import format_json
+        return format_json
+    elif format_type == 'plain':
+        from gendiff.formatters.plain import format_plain
+        return format_plain
+    elif format_type == 'stylish':
+        from gendiff.formatters.stylish import format_stylish
+        return format_stylish
+    else:
+        raise ValueError(f"Unknown format type: {format_type}")
