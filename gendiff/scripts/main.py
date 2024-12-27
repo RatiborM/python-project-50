189a833b3710bc1e1de641fdd7af381476b85026
RatiborM <r_morkvin@vk.com>
2024-12-27 18:59:22 +0300

    исправляю ошибки3

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,12 +1,13 @@
 from gendiff.cli import parse
 from gendiff.diff_with_formatter import generate_diff
 
+
 def main():
     args = parse()
     print(
         generate_diff(args.first_file, args.second_file, formater=args.format)
     )
 
+
 if __name__ == "__main__":
     main()
-
