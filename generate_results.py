26d56508c88afa418a048593073dde24d02a697a
RatiborM <r_morkvin@vk.com>
2024-12-27 19:52:11 +0300

    исправляю ошибки6

---
+++ generate_results.py
@@ -0,0 +1,13 @@
+from gendiff.diff_with_formatter import generate_diff
+
+
+def generate_expected_results():
+    # Передайте пути к файлам
+    file1_path = 'file1.json'
+    file2_path = 'file2.json'
+
+    stylish_result = generate_diff(file1_path, file2_path, formater='stylish')
+
+    # Сохранение результата
+    with open('tests/correct_result.txt', 'w') as file:
+        file.write(stylish_result)
