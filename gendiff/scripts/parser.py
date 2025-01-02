7fc73b4f3912bcfe0dcc2c59da74203bd84f90a5
RatiborM <r_morkvin@vk.com>
2025-01-02 21:24:07 +0300

    uw 4 шаг

--- gendiff/scripts/parser.py
+++ gendiff/scripts/parser.py
@@ -1,25 +1,14 @@
-import json
 import os
-
-def read_json(file_path):
-    """Читает и парсит JSON-файл."""
-    with open(file_path, 'r') as file:
-        return json.load(file)
+from gendiff import generate_diff
 
 # Путь к директории, содержащей JSON-файлы
 base_dir = os.path.dirname(os.path.abspath(__file__))
-project_dir = os.path.abspath(os.path.join(base_dir, '../../test/fixtures'))
+fixtures_dir = os.path.abspath(os.path.join(base_dir, '../../test/fixtures'))
 
 # Путь к файлам
-file1_path = os.path.join(project_dir, 'file1.json')
-file2_path = os.path.join(project_dir, 'file2.json')
+file1_path = os.path.join(fixtures_dir, 'file1.json')
+file2_path = os.path.join(fixtures_dir, 'file2.json')
 
-# Чтение и парсинг файлов
-data1 = read_json(file1_path)
-data2 = read_json(file2_path)
-
-# Вывод данных для проверки
-print("Содержимое file1.json:")
-print(data1)
-print("\nСодержимое file2.json:")
-print(data2)
+# Генерация и вывод диффа
+diff = generate_diff(file1_path, file2_path)
+print(diff)
