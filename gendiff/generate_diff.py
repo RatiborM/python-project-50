0595c56b2bc9abf9a94b8b417d723f7254e4999d
RatiborM <r_morkvin@vk.com>
2025-02-01 19:06:24 +0300

    исправления с Полиной3

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,7 +3,7 @@ from gendiff.parser import read_file, get_data_format, parse_data
 from gendiff.formatters import get_format
 
 
-def generate_diff(file1_path, file2_path, formatter='stylish'):
+def generate_diff(file1_path, file2_path, format_name='stylish'):
     file1_data = read_file(file1_path)
     file2_data = read_file(file2_path)
 
@@ -13,5 +13,5 @@ def generate_diff(file1_path, file2_path, formatter='stylish'):
     data1 = parse_data(file1_data, file1_format)
     data2 = parse_data(file2_data, file2_format)
 
-    formatter = get_format(formatter)
+    formatter = get_format(format_name)
     return formatter(get_diff(data1, data2))
